71f489aed690201e2c5bbf5ae73bccdff63addcd
june050312 <june050312@gmail.com>
2025-11-13 06:08:36 +0900

    11주차 실습

--- be/search/app.js
+++ be/search/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express()
+const path = require('path');
 
 const port = process.env.PORT || 8080
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
