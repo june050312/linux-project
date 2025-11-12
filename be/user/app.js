d2521468ef6385d070c2681bb763868695fc9790
june050312 <june050312@gmail.com>
2025-11-12 16:55:10 +0900

    register

--- be/user/app.js
+++ be/user/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
+const { 
+  getUser, 
+  createUser, 
+  loginUser,
+  deleteUser
+} = require('./controller')
 
 const port = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/api/user', getUser)
+app.post('/api/user/register', createUser)
+app.post('/api/user/login', loginUser)
+app.delete('/api/user/delete', deleteUser)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
